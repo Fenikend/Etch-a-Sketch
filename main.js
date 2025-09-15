703f49ade3a709b01e5dd44b25a0c39973bd9e04
Fenikend <nichiporuki2003@gmail.com>
2025-09-15 14:33:46 +0300

    improve color handling function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const FIELD_SIZE=540
+
 const cont=document.querySelector('#container')
 const btn=document.querySelector('#add')
 const resetBtn=document.querySelector('#reset')
@@ -15,14 +17,14 @@ function setField(val){
     for(let i=0;i<val;i++){
         let row=document.createElement('div')
         row.classList.add('row')
-        let size=Math.ceil(540/val)
+        let size=Math.ceil(FIELD_SIZE/val)
         row.setAttribute('style',`height:${size}px;padding:0;margin:0;`)
         for(let j=0;j<val;j++){
             let pixel=document.createElement('div')
             console.log(val)
             pixel.setAttribute('style',`width:${size}px;`)
             pixel.addEventListener('mouseover',()=>{
-            pixel.style.backgroundColor='black'
+            setColor('black',pixel);
             })
             row.appendChild(pixel)}
         cont.appendChild(row)
@@ -32,15 +34,27 @@ function resetField(){
     let cells=document.querySelectorAll('#container .row div')
     console.log(cells)
     cells.forEach((elem)=>{
-        elem.style.backgroundColor='white';
+        setColor('white',elem);
     
     })
 
 }
-function setColor(){
+//generate random rgb type color 
+function generateColor(){
     const randNum=Math.floor(Math.random()*0xFFFFFF);
-    let hexColor = randomNum.toString(16);
+    let hexColor = randNum.toString(16);
     hexColor = hexColor.padStart(6, '0');
 
     return `#${hexColor}`
+}
+
+function setColor(color,elem){
+    if(color==='rgb'){
+        elem.style.backgroundColor=generateColor();
+    }
+    else{
+        elem.style.backgroundColor=color
+    }
+    
+
 }
